468094a4881548c64ae7a433d33bdf7b06e9413f
victor fernandez <vfernandezg@gmail.com>
2026-01-27 18:58:45 +0100

    first commit

--- src/a2a_protocolo_crewai/tools/__init__.py
+++ src/a2a_protocolo_crewai/tools/__init__.py
@@ -5,7 +5,6 @@ Herramientas personalizadas para el Orquestador Azure A2A.
 from .system_cli import SystemCLITool
 from .azure_cli import AzureCLITool, AzureLoginTool
 from .azure_powershell import AzurePowerShellTool, AzurePSConnectTool
-from .cli_interaction import AskUserTool, DisplayMessageTool
 
 __all__ = [
     "SystemCLITool",
@@ -13,6 +12,4 @@ __all__ = [
     "AzureLoginTool",
     "AzurePowerShellTool",
     "AzurePSConnectTool",
-    "AskUserTool",
-    "DisplayMessageTool",
 ]
